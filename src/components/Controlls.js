e9c70876bb118822ad4ff7481a8399c0e98c4c3b
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-10-04 13:19:46 -0700

    render play/pause icon based on player state

--- src/components/Controlls.js
+++ src/components/Controlls.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function Controller({ onNext, onPrv }) {
+export default function Controller({ skip, prev, playbackState, pause, play }) {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onPrv}>
+      <TouchableOpacity onPress={prev}>
         <MaterialIcons name="skip-previous" size={50} />
       </TouchableOpacity>
       <TouchableOpacity>
-        <MaterialIcons name="pause" size={50} />
+        {playbackState === 'playing' ? (
+          <MaterialIcons name="pause" size={50} onPress={pause} />
+        ) : (
+          <MaterialIcons name="play-arrow" size={50} onPress={play} />
+        )}
       </TouchableOpacity>
-      <TouchableOpacity onPress={onNext}>
+      <TouchableOpacity onPress={skip}>
         <MaterialIcons name="skip-next" size={50} />
       </TouchableOpacity>
     </View>
