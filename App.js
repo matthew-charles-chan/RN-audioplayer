f2cc995d923afaabf29ea3a269044febb524f736
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-10-04 12:09:33 -0700

    add react-native-track-player library

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import episodes from './src/data';
+import TrackPlayer from 'react-native-track-player';
+
 
 export default function App() {
+  useEffect(() => {
+    TrackPlayer.setupPlayer().then(async () => {
+      // The player is ready to be used
+      console.log('Player ready');
+      console.log(episodes);
+      // add the array of songs in the playlist
+      await TrackPlayer.reset();
+      await TrackPlayer.add(episodes[1]);
+      TrackPlayer.play();
+
+      //add listener on track change
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
